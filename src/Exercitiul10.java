c2c1c01c467a43b1d5703e6709e0dadc1c59dd69
Ion Schimbator <ionschimbator98@gmail.com>
2025-11-30 14:05:42 +0200

    Add ex10

---
+++ src/Exercitiul10.java
@@ -0,0 +1,10 @@
+public class Exercitiul10 {
+    public static void main(String[] args) {
+
+        for (int i = 18; i <= 32; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + ", ");
+            }
+        }
+    }
+}

78b6b007d9cdc365e998c80ba10764de5c1b855d
Ion Schimbator <ionschimbator98@gmail.com>
2025-11-30 13:20:31 +0200

    Add ex7

---
+++ src/Exercitiul7.java
@@ -0,0 +1,10 @@
+public class Exercitiul7 {
+    public static void main(String[] args) {
+
+        String text = "      Java programming        ";
+        System.out.println(text);
+
+        String result = text.trim();
+        System.out.println(result);
+    }
+}

a01e6e379cef4a0c46e86d8d19e40c413d0771f7
Ion Schimbator <ionschimbator98@gmail.com>
2025-11-30 14:01:49 +0200

    Add ex9

---
+++ src/Exercitiul9.java
@@ -0,0 +1,9 @@
+import Method.CalculatePerimeter;
+
+public class Exercitiul9 {
+    public static void main(String[] args) {
+
+        int perimeter = new CalculatePerimeter().CalcPerimeter();
+        System.out.println("The perimeter of the quadrilateral is: " + perimeter + " cm");
+    }
+}
---
+++ src/Method/CalculatePerimeter.java
@@ -0,0 +1,9 @@
+package Method;
+
+public class CalculatePerimeter {
+
+    public int CalcPerimeter() {
+        int side = 8;
+        return 4 * side;
+    }
+}

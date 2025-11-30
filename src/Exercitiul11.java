629714e4e46bf2375d09622ef3caa7789415dcea
Ion Schimbator <ionschimbator98@gmail.com>
2025-11-30 14:09:04 +0200

    Add ex11

---
+++ src/Exercitiul11.java
@@ -0,0 +1,11 @@
+public class Exercitiul11 {
+    public static void main(String[] args) {
+
+        int x = 7;
+
+        do {
+            System.out.println("This is executed only once, even if the condition is false!");
+            x++;
+        } while (x < 5);
+    }
+}
